2632ea8bf75cbf99b49b798943c27faa16c2f9b4
VladThodo <vladtodosin@gmail.com>
2016-02-18 13:51:48 +0200

    Removed comments from file :)

--- app/src/main/java/com/vlath/beheexplorer/TabAdapter.java
+++ app/src/main/java/com/vlath/beheexplorer/TabAdapter.java
@@ -29,19 +29,19 @@ public class TabAdapter extends BaseAdapter{
     }
     @Override
     public int getCount() {
-        // TODO Auto-generated method stub
+       
         return 1;
     }
 
     @Override
     public Object getItem(int position) {
-        // TODO Auto-generated method stub
+        
         return position;
     }
 
     @Override
     public long getItemId(int position) {
-        // TODO Auto-generated method stub
+     
         return position;
     }
     public class Holder
@@ -52,7 +52,7 @@ public class TabAdapter extends BaseAdapter{
     }
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
-        // TODO Auto-generated method stub
+        
         Holder holder=new Holder();
 
         rowView = inflater.inflate(R.layout.tab_item,null);
@@ -74,7 +74,7 @@ public class TabAdapter extends BaseAdapter{
         rowView.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
+             
                 Toast.makeText(context, "You Clicked " + names.get(position), Toast.LENGTH_LONG).show();
             }
         });
