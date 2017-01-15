32a57e7d96189b3767d925ff319a65c1dc96643d
Vlad Todosin <vladtodosin@gmail.com>
2017-01-15 14:13:03 +0200

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+
+jQuery(document).ready(function() {
+	
+    /*
+        Fullscreen background
+    */
+    $('.top-content').backstretch("assets/img/backgrounds/1.jpg");
+    $('.single-feature-bg').backstretch("assets/img/backgrounds/1.jpg");
+    
+});
+
